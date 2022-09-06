c4abf4161816b4f98978f2de45affc1bdc32e25b
Liz2208 <108889788+Liz2208@users.noreply.github.com>
2022-09-06 16:04:52 -0500

    primeros botones

---
+++ src/components/welcome.js
@@ -0,0 +1,12 @@
+export const welcome = () => {
+const div= document.createElement('div');    
+const buttonLogin = document.createElement('button');
+const buttonRegister = document.createElement('button');
+
+buttonLogin.textContent = 'Inicia Sesión';
+buttonRegister.textContent = 'Registrate';
+
+div.append(buttonLogin, buttonRegister);
+
+return div;
+};
