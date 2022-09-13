00afe0dbc3eb9cb3c974b1c4641c3a549380e66d
janethpc <108207854+janethpc@users.noreply.github.com>
2022-09-12 21:24:49 -0500

    primera implementacion de firebase

--- src/components/register.js
+++ src/components/register.js
@@ -1,4 +1,26 @@
 import { onNavigate } from '../main.js';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC2examG0LregGs2C9N9FGt4a7SFb420u4",
+  authDomain: "party-girls-2022.firebaseapp.com",
+  projectId: "party-girls-2022",
+  storageBucket: "party-girls-2022.appspot.com",
+  messagingSenderId: "553450782361",
+  appId: "1:553450782361:web:a60f6128e6d42e5a5d4208",
+  measurementId: "G-LKR9GJXQYC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 export const register = () => {
   const div = document.createElement('div');
@@ -31,7 +53,7 @@ export const register = () => {
   buttonLoginOne.textContent = 'Create acount';
   buttonBack.textContent = 'Back';
   inputEmail.placeholder = 'Email';
-  inputPassword.placeholder = 'Pasword';
+  inputPassword.placeholder = 'Password';
   logoGoogle.src = './images/btn_google.png';
   logoTwitter.src = './images/btn_twitter.png';
   logogithub.src = './images/git.png';
