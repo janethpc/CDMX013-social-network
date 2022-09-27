901e43b5ab86517e37de350422346bc566d32f63
janethpc <108207854+janethpc@users.noreply.github.com>
2022-09-27 14:05:16 -0500

    conexion firebase

---
+++ src/lib/posts.js
@@ -0,0 +1,22 @@
+import { collection, addDoc, getDocs } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js';
+import { db } from './config.js';
+
+
+export const savePost = async (text) => {
+  if (text != '') {
+    const docRef = await addDoc(collection(db, 'posts'), {
+      texto: text,
+      likes: [],
+      // fecha
+      // correo
+    });
+    console.log('Document written with ID: ', docRef);
+    if (docRef.id != '') {
+      console.log('se guardo');
+    }
+  } else {
+    console.error('Ojoo checa tu text! ');
+  }
+};
+
+export const getTask = () => getDocs(collection(db, 'posts'));
