d939726cb3b3030f76cf8e2c4facd8ff11feefcc
Liz2208 <108889788+Liz2208@users.noreply.github.com>
2022-10-05 13:33:42 -0500

    botn de editar aun sin funcion

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,19 @@
 import { cerrarsesion, verUsuario } from '../lib/auth.js';
-import { savePost, getPost, deletePost } from '../lib/posts.js';
+import {
+  savePost, getPost, deletePost, getTask, updatePost
+} from '../lib/posts.js';
 
 const tasksContainer = document.createElement('taskContainer');
 tasksContainer.id = 'taskContainer';
 
-export const home = () => {
+let editStatus = false;
 
-  const imprimirPost = getPost((querySnapshot)=>{
-    console.log("pintado de posts");
-    let html = "";
+let id =""
+
+export const home = () => {
+  const imprimirPost = getPost((querySnapshot) => {
+    console.log('pintado de posts');
+    let html = '';
     querySnapshot.forEach((doc) => {
       const task = doc.data();
       html += `
@@ -16,19 +21,32 @@ export const home = () => {
       <p>${task.texto}</p>
       </section>
       <img src='./images/borrar.png' id='btnDelete' class='btnDelete' data-id='${doc.id}' ></img>
-    <img src='./images/hearts.png' id='heart' ></img>
+      <img src='./images/hearts.png' id='heart' ></img>
+      <img src='./images/editar.png' id='btnEdit' class='btnEdit' data-id='${doc.id}' ></img>
     `;
-  });
-  tasksContainer.innerHTML = html;
-  const btnsDelete = tasksContainer.querySelectorAll('.btnDelete');
-  btnsDelete.forEach((btn) => {
-    btn.addEventListener('click', ({ target: { dataset } }) => {
-      console.log(dataset.id);
-      deletePost(dataset.id);
+    });
+    tasksContainer.innerHTML = html;
+    const btnsDelete = tasksContainer.querySelectorAll('.btnDelete');
+    btnsDelete.forEach((btn) => {
+      btn.addEventListener('click', ({ target: { dataset } }) => {
+        console.log(dataset.id);
+        deletePost(dataset.id);
+      });
+    });
+
+    /* boton editar */
+    const btnsEdit = tasksContainer.querySelectorAll('.btnEdit');
+    btnsEdit.forEach((btn) => {
+      btn.addEventListener('click', async (e) => {
+        const doc = await getTask(e.target.dataset.id);
+        const task = doc.data();
+        console.log(doc.data());
+        document.getElementById('inputPost').value = task; /*aqui se selecciona y se pinta en input */
+        editStatus = true;
+      });
     });
   });
-});
- 
+
   const container = document.createElement('div');
   container.id = 'container';
 
@@ -42,12 +60,9 @@ export const home = () => {
   const usuario = verUsuario();
   console.log(usuario);
 
- const usuario = verUsuario();
-  console.log(usuario);
-
-  //const verEmail = usuario.email;
+  // const verEmail = usuario.email;
   const greeting = document.createElement('p');
-  greeting.textContent = "Hola";
+  greeting.textContent = 'Hola';
   greeting.id = 'titlePost';
 
   const logOut = document.createElement('img');
@@ -74,8 +89,16 @@ export const home = () => {
   buttonPost.className = 'buttonPost';
   buttonPost.textContent = 'Post';
   buttonPost.addEventListener('click', async () => {
-    await savePost(inputPost.value);
+    document.getElementById('inputPost').value = "";
+    if (!editStatus) {
+      await savePost(inputPost.value);
+    } else {
+      updatePost();
+      editStatus = false;
+    }
+    inputPost.reset();
   });
+
   const divWall = document.createElement('div');
   divWall.id = 'divWall';
   divWall.appendChild(tasksContainer);
--- src/lib/posts.js
+++ src/lib/posts.js
@@ -1,8 +1,7 @@
 import {
-  collection, doc, addDoc, deleteDoc, onSnapshot
+  collection, doc, addDoc, deleteDoc, onSnapshot, getDoc, updateDoc,
 } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js';
 import { db } from './config.js';
-
 
 export const savePost = async (text) => {
   if (text != '') {
@@ -26,3 +25,7 @@ export const getPost = (callback) => {
 };
 
 export const deletePost = (id) => deleteDoc(doc(db, 'posts', id));
+
+export const getTask = (id) => getDoc(doc(db, 'posts', id));
+
+export const updatePost = (id, newFields) => updateDoc(doc(db, 'posts', id), newFields);
