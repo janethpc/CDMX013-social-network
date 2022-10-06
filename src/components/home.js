2b0dbc4e52167c4f54c82137aa2adc33d294a816
Katya Laso <kat_jenny@hotmail.com>
2022-10-06 13:38:35 -0500

    cambios css en home

--- src/components/home.js
+++ src/components/home.js
@@ -1,17 +1,18 @@
 import { cerrarsesion, verUsuario } from '../lib/auth.js';
-import { savePost, getPost, deletePost, getTask, updatePost } from '../lib/posts.js';
+import {
+  savePost, getPost, deletePost, getTask, updatePost,
+} from '../lib/posts.js';
 
 const tasksContainer = document.createElement('taskContainer');
 tasksContainer.id = 'taskContainer';
 
 let editStatus = false;
-let id ="";
+let id = '';
 
 export const home = () => {
-
-  const imprimirPost = getPost((querySnapshot)=>{
-    console.log("pintado de posts");
-    let html = "";
+  const imprimirPost = getPost((querySnapshot) => {
+    console.log('pintado de posts');
+    let html = '';
     querySnapshot.forEach((doc) => {
       const task = doc.data();
       html += `
@@ -23,18 +24,18 @@ export const home = () => {
     <p id="counterLikes" class="counterLikes">${task.likes}</p>
     <img src='./images/editar.png' id='btnEdit' class='btnEdit' data-id='${doc.id}' ></img>
     `;
-    console.log(task);
-  });
-  tasksContainer.innerHTML = html;
-  const btnsDelete = tasksContainer.querySelectorAll('.btnDelete');
-  btnsDelete.forEach((btn) => {
-    btn.addEventListener('click', ({ target: { dataset } }) => {
-      console.log(dataset.id);
-      deletePost(dataset.id);
+      console.log(task);
     });
-  });
-   /* boton editar */
-   const btnsEdit = tasksContainer.querySelectorAll('.btnEdit');
+    tasksContainer.innerHTML = html;
+    const btnsDelete = tasksContainer.querySelectorAll('.btnDelete');
+    btnsDelete.forEach((btn) => {
+      btn.addEventListener('click', ({ target: { dataset } }) => {
+        console.log(dataset.id);
+        deletePost(dataset.id);
+      });
+    });
+    /* boton editar */
+    const btnsEdit = tasksContainer.querySelectorAll('.btnEdit');
     btnsEdit.forEach((btn) => {
       btn.addEventListener('click', async (e) => {
         const doc = await getTask(e.target.dataset.id);
@@ -44,23 +45,23 @@ export const home = () => {
         id = doc.id;
       });
     });
-   /*boton like*/
-   const btnsHearts = tasksContainer.querySelectorAll('.heart');
-   btnsHearts.forEach((btn) => {
-    btn.addEventListener('click', async (e) => {
-    const doc2 = await getTask(e.target.dataset.id); // acceder al objeto que contiene identificador especifico
-    const likesEdit = doc2.data();
-    for (let i = 0; i < likesEdit; i++) {
-      counterLikes.value = i;
-      counterLikes.textContent = counterLikes.value;
-      console.log(counterLikes.value);
-      
-      updatePost(doc2.id, { likes: counterLikes.value });
-    }
+    /* boton like */
+    const btnsHearts = tasksContainer.querySelectorAll('.heart');
+    btnsHearts.forEach((btn) => {
+      btn.addEventListener('click', async (e) => {
+        const doc2 = await getTask(e.target.dataset.id); // acceder al objeto que contiene identificador especifico
+        const likesEdit = doc2.data();
+        for (let i = 0; i < likesEdit; i++) {
+          counterLikes.value = i;
+          counterLikes.textContent = counterLikes.value;
+          console.log(counterLikes.value);
+
+          updatePost(doc2.id, { likes: counterLikes.value });
+        }
+      });
     });
   });
-})
- 
+
   const container = document.createElement('div');
   container.id = 'container';
 
@@ -71,14 +72,10 @@ export const home = () => {
   logoHorizontal.src = './images/logoh.png';
   logoHorizontal.id = 'logoHorizontal';
 
- const usuario = verUsuario();
+  const usuario = verUsuario();
   console.log(usuario);
 
-  //const verEmail = usuario.email;
-  const greeting = document.createElement('p');
-  greeting.textContent = "Hi, do you want to share something?";
-  greeting.id = 'titlePost';
-
+  // const verEmail = usuario.email;
   const logOut = document.createElement('img');
   logOut.src = './images/cerrar.png';
   logOut.id = 'logOut';
@@ -88,6 +85,10 @@ export const home = () => {
 
   const divPost = document.createElement('div');
   divPost.id = 'divPost';
+
+  const greeting = document.createElement('p');
+  greeting.textContent = 'Hi, do you want to share something?';
+  greeting.id = 'titlePost';
 
   const inputPost = document.createElement('input');
   inputPost.className = 'inputPost';
@@ -102,7 +103,7 @@ export const home = () => {
     if (!editStatus) {
       await savePost(inputPost.value);
     } else {
-      updatePost(id, {texto: inputPost.value});
+      updatePost(id, { texto: inputPost.value });
       editStatus = false;
       buttonPost.src = './images/editar.png';
     }
@@ -114,7 +115,7 @@ export const home = () => {
   divWall.appendChild(tasksContainer);
 
   container.append(divHeader, divPost, divWall);
-  divHeader.append(logoHorizontal, greeting, logOut);
-  divPost.append(inputPost, buttonPost);
+  divHeader.append(logoHorizontal, logOut);
+  divPost.append(greeting, inputPost, buttonPost);
   return container;
 };
